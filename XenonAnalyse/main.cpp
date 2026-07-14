94c2b2fb8567892e8bd36568607091bfd7b49bb2
Ricardo Baucells <ricardo@baucells.net>
2026-07-14 12:16:34 -0400

    cmplwi and bgt are actually part of the switch type 1

--- XenonAnalyse/main.cpp
+++ XenonAnalyse/main.cpp
@@ -247,6 +247,8 @@ int main(int argc, char** argv)
 
     uint32_t switch1[] =
     {
+        PPC_INST_CMPLWI,
+        PPC_INST_BGT,
         PPC_INST_LIS,
         PPC_INST_RLWINM,
         PPC_INST_ADDI, // or subi i guess cuz subi = addi with -value
