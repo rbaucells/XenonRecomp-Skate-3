b10871198d8c936cb39c5e6e1f64ed7e6df6ea00
Ricardo Baucells <ricardo@baucells.net>
2026-07-14 11:02:59 -0400

    portability fixes

--- XenonUtils/byteswap.h
+++ XenonUtils/byteswap.h
@@ -1,18 +1,44 @@
-#pragma once 
+#pragma once
 
 #include <cassert>
+#include <cstdint>
+#if defined(_MSC_VER)
+#include <intrin.h>
+#endif
 
 template<typename T>
 inline T ByteSwap(T value)
 {
-    if constexpr (sizeof(T) == 1)
+    if constexpr (sizeof(T) == 1) {
         return value;
-    else if constexpr (sizeof(T) == 2)
-        return static_cast<T>(__builtin_bswap16(static_cast<uint16_t>(value)));
-    else if constexpr (sizeof(T) == 4)
-        return static_cast<T>(__builtin_bswap32(static_cast<uint32_t>(value)));
-    else if constexpr (sizeof(T) == 8)
-        return static_cast<T>(__builtin_bswap64(static_cast<uint64_t>(value)));
+    }
+    else if constexpr (sizeof(T) == 2) {
+#if defined(__has_builtin)
+#if __has_builtin(__builtin_bswap16)
+        return static_cast<T>(__builtin_bswap16(static_cast<std::uint16_t>(value)));
+#endif
+#elif defined(_MSC_VER)
+        return static_cast<T>(_byteswap_ushort(static_cast<std::uint16_t>(value)));
+#endif
+    }
+    else if constexpr (sizeof(T) == 4) {
+#if defined(__has_builtin)
+#if __has_builtin(__builtin_bswap32)
+        return static_cast<T>(__builtin_bswap32(static_cast<std::uint32_t>(value)));
+#endif
+#elif defined(_MSC_VER)
+        return static_cast<T>(_byteswap_ulong(static_cast<std::uint32_t>(value)));
+#endif
+    }
+    else if constexpr (sizeof(T) == 8) {
+#if defined(__has_builtin)
+#if __has_builtin(__builtin_bswap64) // Fixed typo here from bswap32 to bswap64
+        return static_cast<T>(__builtin_bswap64(static_cast<std::uint64_t>(value)));
+#endif
+#elif defined(_MSC_VER)
+        return static_cast<T>(_byteswap_uint64(static_cast<std::uint64_t>(value)));
+#endif
+    }
 
     assert(false && "Unexpected byte size.");
     return value;
