f599bc1e0162ca0f45ebb06ba130cc01fd08c8bc
Ricardo Baucells <ricardo@baucells.net>
2026-07-18 22:44:03 -0400

    stfsux

--- XenonRecomp/recompiler.cpp
+++ XenonRecomp/recompiler.cpp
@@ -1596,6 +1596,12 @@ bool Recompiler::Recompile(
         println("\t{}.u32 = {};", r(insn.operands[2]), ea());
         break;
 
+    case PPC_INST_STFSUX:
+        println("\t{} = {}.u32 + {}.u32;", ea(), r(insn.operands[2]), r(insn.operands[1]));
+        println("\tPPC_STORE_U32({}, *reinterpret_cast<uint32_t*>(&{}.f32));", ea(), f(insn.operands[0]));
+        println("\t{}.u32 = {};", r(insn.operands[2]), ea());
+        break;
+
     case PPC_INST_STFSX:
         printSetFlushMode(false);
         println("\t{}.f32 = float({}.f64);", temp(), f(insn.operands[0]));
