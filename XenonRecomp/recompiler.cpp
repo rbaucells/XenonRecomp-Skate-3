6d1bb6d5e4a81d274b0bd86b07ab65ff903259cf
Ricardo Baucells <ricardo@baucells.net>
2026-08-19 18:13:53 -0400

    mixed destination operand with index in vcmpbfp128

--- XenonRecomp/recompiler.cpp
+++ XenonRecomp/recompiler.cpp
@@ -1998,7 +1998,7 @@ bool Recompiler::Recompile(
         // TODO: vectorize
         for (int i = 0; i < 4; i++) {
             println("\t{0}.u32[{1}] = 0;", v(insn.operands[0]), i);
-            println("\tif (isnan({1}.f32[{0}]) || isnan({2}.f32[{0}])) {0}.u32[{0}] = 0xC0000000;", i, v(insn.operands[1]), v(insn.operands[2]));
+            println("\tif (isnan({1}.f32[{0}]) || isnan({2}.f32[{0}])) {3}.u32[{0}] = 0xC0000000;", i, v(insn.operands[1]), v(insn.operands[2]), v(insn.operands[0]));
             println("\telse {{");
             println("\t\tif ({1}.f32[{0}] > {2}.f32[{0}]) {3}.u32[{0}] |= 0x80000000;", i, v(insn.operands[1]), v(insn.operands[2]), v(insn.operands[0]));
             println("\t\tif ({1}.f32[{0}] < -{2}.f32[{0}]) {3}.u32[{0}] |= 0x40000000;", i, v(insn.operands[1]), v(insn.operands[2]), v(insn.operands[0]));
